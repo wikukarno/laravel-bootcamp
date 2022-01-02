1af3b1c41d0b672e8d6afbd52b5f42a939a7267c
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2022-01-02 19:45:51 +0700

    update Checkout model

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Checkout extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'camp_id',
+        'card_number',
+        'expired',
+        'cvc',
+        'is_paid'
+    ];
 }
